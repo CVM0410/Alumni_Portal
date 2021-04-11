30254db941aa3018195d257491efc08e7a067cd2
Neha Sawant <67453268+SNeha9920@users.noreply.github.com>
2021-04-11 23:11:29 +0530

    Update action2.php

--- Admin/action2.php
+++ Admin/action2.php
@@ -2,14 +2,14 @@
 if ($_POST['action'] && $_POST['id']) {
   if ($_POST['action'] == 'Approve') {
   $id = $_POST['id'];
-  $connect = mysqli_connect("localhost", "Neha", "1235", "test");  
+  $connect = mysqli_connect("localhost", "root", "", "test");  
   $qry = "UPDATE posts SET approval_status = '1' WHERE id='$id'";
   $res = mysqli_query($connect, $qry);
   header("location:approve.php");
   }
   if ($_POST['action'] == 'Not Approve') {
   $id = $_POST['id'];
-  $connect = mysqli_connect("localhost", "Neha", "1235", "test");  
+  $connect = mysqli_connect("localhost", "root", "", "test");  
   $qry = "UPDATE posts SET approval_status = '0' WHERE id='$id'";
   $res = mysqli_query($connect, $qry);
   header("location:approve.php");
